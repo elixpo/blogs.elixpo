1581cc586fe80a6ab98d7e4c8c1a71f5d263bbd0
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-03-29 19:10:40 +0530

    fixed the migratin

--- migrations/0006_blog_collab_roles.sql
+++ migrations/0006_blog_collab_roles.sql
@@ -1,5 +1,2 @@
 -- Add role column to blog_co_authors (viewer / editor / admin)
 ALTER TABLE blog_co_authors ADD COLUMN role TEXT NOT NULL DEFAULT 'editor';
-
--- Add page_emoji to blogs for the emoji icon above the title
-ALTER TABLE blogs ADD COLUMN page_emoji TEXT DEFAULT '';
