460e9f2bb99ca8e0f7d896bcef68f413e92a4b0f
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-03-31 14:32:02 +0530

    updated org invite email template

--- email/test.js
+++ email/test.js
@@ -28,8 +28,11 @@ const SAMPLES = {
     orgLogoUrl: '',
     inviterName: 'selenium-cutlet',
     inviterAvatar: 'https://lh3.googleusercontent.com/a/ACg8ocJlCSczaUeHuK0Vu79xKBwghqBj60V9evGwmf0aO_k36BCZ1aE=s96-c',
+    recipientName: 'Ada Lovelace',
+    recipientAvatar: '',
     role: 'write',
     inviteUrl: 'https://blogs.elixpo.com/org/join/abc123',
+    orgUrl: 'https://blogs.elixpo.com/elixpo',
     message: 'Hey! Come write some cool blog posts with us.',
   }),
 
